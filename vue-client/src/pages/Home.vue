c17277641cbbd00a49092d742a69babe105fe02b
finmauriciosilvak3-crypto <finmauriciosilvak3@gmail.com>
2025-07-29 21:30:53 -0300

    Update gitignore

---
+++ vue-client/src/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Dashboard Stats</h1>
+    <div v-if="stats">
+      <p>Contracts: {{ stats.contracts }}</p>
+      <p>Payments Due: {{ stats.payments_due }}</p>
+      <p>Expenses: {{ stats.expenses }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const stats = ref(null)
+
+onMounted(async () => {
+  const res = await axios.get('/api/stats')
+  stats.value = res.data
+})
+</script>
